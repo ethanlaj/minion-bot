0948346646b1e9c6e913e390af1031568903ec89
ethanlaj <ethanlaj@ericlaj.com>
2018-10-28 21:58:57 -0400

    finish up bot

--- commands/dmall.js
+++ commands/dmall.js
@@ -1,21 +1,38 @@
 var Discord = require("discord.js");
 module.exports = {
-	run: function (bot, message, args) {
-		var announceChannel = bot.channels.get(this.settings.announceChannel);
-		if (!message.member.roles.has(this.settings.requiredRole)) return message.reply("You do not have permission to use this command!");
+	run: async function (bot, message, args) {
+		//if (!message.member.roles.has(this.settings.requiredRole)) return message.reply("You do not have permission to use this command!");
+		if (this.settings.running) return message.reply("This bot is already dming all members of this server, please try again later.");
 		if (!args[0]) return message.reply(`You did not supply enough parameters. Usage: \`${this.settings.usage}\``);
+		var members = message.guild.members.array().filter((m) => !m.user.bot);
 		var announceEmbed = new Discord.RichEmbed()
-			.setColor("#f5f5dc")
-			.addField("New Announcement", args.join(" "))
-			.addField("Announced By:", `\n${message.author.toString()}`)
-			.setFooter(message.author.tag);
-		announceChannel.send("@here", { embed: announceEmbed, disableEveryone: false }).catch(() => {
-			message.reply("I do not have permissions to send messages in the announcements channel");
+			.setColor("BLUE")
+			.setTitle("Message from Angelina")
+			.setDescription(args.join(" "));
+		var i = 0;
+		await message.reply("\nYou are about to send a message that looks like the one below to ALL members\nType **CONFIRM** to confirm.\nType anything else to cancel.\nOnce started, this cannot be stopped.", {embed: announceEmbed});
+		message.channel.awaitMessages((m) => m.author.id === message.author.id, { maxMatches: 1, time: 15000, errors: ["time"] }).then((collected) => {
+			if (collected.first().content !== "CONFIRM") return message.reply("Cancelled prompt.");
+			this.settings.running = true;
+			message.reply("Okay, I started dming all members of this server.");
+			async function dmMember() {
+				if (i <= members.length - 1) {
+					await members[i].send(announceEmbed).catch(() => {});
+					i++;
+					setTimeout(dmMember, 10000);
+				} else {
+					module.exports.settings.running = false;
+				}
+			}
+			setTimeout(dmMember, 1000);
+		}).catch(() => {
+			message.channel.send("Prompt was cancelled because there was no response for 15 seconds.");
 		});
 	},
 	settings: {
 		name: "dmall",
-		usage: "-dmall (dm)",
+		usage: "!dmall (dm)",
 		requiredRole: "",
+		running: false
 	}
 };
